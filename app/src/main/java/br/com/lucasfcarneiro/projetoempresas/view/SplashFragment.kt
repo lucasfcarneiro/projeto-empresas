c354bdbe01a242dae902e6b5fa9862da0369f42c
lukasfcarneiro <lukasfcarneiro@gmail.com>
2020-02-27 22:30:57 -0300

    Navigating to login fragment

--- app/src/main/java/br/com/lucasfcarneiro/projetoempresas/view/SplashFragment.kt
+++ app/src/main/java/br/com/lucasfcarneiro/projetoempresas/view/SplashFragment.kt
@@ -2,10 +2,13 @@ package br.com.lucasfcarneiro.projetoempresas.view
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import br.com.lucasfcarneiro.projetoempresas.R
 
@@ -14,10 +17,18 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
+        }, 1000)
+    }
 
 }
