617e80f10e6470a03cb1d9d3be91f55f863903fd
lukasfcarneiro <lukasfcarneiro@gmail.com>
2020-03-17 21:18:57 -0300

    Layout home toolbar finished

--- app/src/main/java/br/com/lucasfcarneiro/projetoempresas/view/HomeFragment.kt
+++ app/src/main/java/br/com/lucasfcarneiro/projetoempresas/view/HomeFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.SearchView
+import androidx.core.view.isGone
 import br.com.lucasfcarneiro.projetoempresas.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.search_bar.*
 
 
 class HomeFragment : Fragment() {
@@ -16,9 +19,27 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        homeSearchView.setOnSearchClickListener{
+            homeLogo.visibility = View.GONE
+            searchStartHint.visibility = View.GONE
+            homeSearchView.maxWidth = Integer.MAX_VALUE
+        }
+
+        homeSearchView.setOnCloseListener {
+            homeLogo.visibility = View.VISIBLE
+            searchStartHint.visibility = View.VISIBLE
+            false
+        }
+    }
 
 }
